5e420f976c4dd52b7f939193984298902dfb4956
12138 <drelf2018@outlook.com>
2026-01-17 01:55:54 +0800

    fix: 优化钉钉机器人日志模板

--- hook/dingtalk.go
+++ hook/dingtalk.go
@@ -9,22 +9,37 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// DingTalk 钉钉
 const DingTalk string = "dingtalk"
 
-var dingtalkTmpl, _ = template.New("dingtalk").
-	Funcs(template.FuncMap{
-		"upper": strings.ToUpper,
-		"base":  filepath.Base,
-	}).
-	Parse(`### [{{upper .Level.String}}] {{if .Data.title}}{{.Data.title}}{{else}}{{base .Caller.File}}{{end}}
+// DingTalkMarkdown 钉钉 markdown 消息模板
+const DingTalkMarkdown string = `### [{{upper .Level.String}}] {{if .Data.title}}{{.Data.title}}{{else}}{{base .Caller.File}}{{end}}
 
-{{.Message}}
+#### {{.Message}}
 
-###### {{.Time.Format "2006-01-02 15:04:05"}}`)
+###### {{.Time.Format "2006-01-02 15:04:05"}}`
 
+// DingTalkTemplateFunc 钉钉 markdown 消息模板中使用到的函数
+var DingTalkTemplateFunc = template.FuncMap{"upper": strings.ToUpper, "base": filepath.Base}
+
+// DingTalkTemplate 钉钉模板
+var DingTalkTemplate = template.Must(template.New(DingTalk).Funcs(DingTalkTemplateFunc).Parse(DingTalkMarkdown))
+
+// MarkdownReplacer 替换 markdown 部分符号
+var MarkdownReplacer = strings.NewReplacer("# ", "", "#", "", "\n\n", "\n")
+
+// DingTalkHook 钉钉机器人钩子
 type DingTalkHook struct {
 	*dingtalk.Bot
 	levels []logrus.Level // 日志等级，为空时视为全部等级
+}
+
+// SendMarkdown 发送 markdown 消息，使用替换部分符号的文本作为标题，发送失败时会记录错误
+func (d *DingTalkHook) SendMarkdown(logger *logrus.Logger, text string) {
+	err := d.Bot.SendMarkdown(" "+MarkdownReplacer.Replace(text), text)
+	if err != nil {
+		logger.Error(err)
+	}
 }
 
 func (d *DingTalkHook) Levels() []logrus.Level {
@@ -37,21 +52,12 @@ func (d *DingTalkHook) Levels() []logrus.Level {
 func (d *DingTalkHook) Fire(entry *logrus.Entry) error {
 	if data, ok := entry.Data[DingTalk]; ok {
 		if data, ok := data.(string); ok && data == d.Name {
-			delete(entry.Data, DingTalk)
 			var b strings.Builder
-			err := dingtalkTmpl.Execute(&b, entry)
+			err := DingTalkTemplate.Execute(&b, entry)
 			if err != nil {
 				return err
 			}
-			go func(logger *logrus.Logger, text string) {
-				title := strings.ReplaceAll(text, "\n\n", "\n")
-				title = strings.ReplaceAll(title, "\n", " ")
-				title = strings.ReplaceAll(title, "#", "")
-				err := d.SendMarkdown(title, text)
-				if err != nil {
-					logger.Error(err)
-				}
-			}(entry.Logger, b.String())
+			go d.SendMarkdown(entry.Logger, b.String())
 		}
 	}
 	return nil
