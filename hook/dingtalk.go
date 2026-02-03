0e483462e05c402dacc2c44ee24a31043bfc3c94
12138 <41439182+Drelf2018@users.noreply.github.com>
2026-02-03 15:17:05 +0800

    fix: 功能拆分

--- hook/dingtalk.go
+++ hook/dingtalk.go
@@ -1,12 +1,15 @@
 package hook
 
 import (
+	"encoding/json"
+	"fmt"
 	"path/filepath"
 	"strings"
 	"text/template"
 	"time"
 
 	"github.com/Drelf2018/dingtalk"
+	"github.com/Drelf2018/exp/filler"
 	"github.com/sirupsen/logrus"
 	stripmd "github.com/writeas/go-strip-markdown"
 )
@@ -14,67 +17,47 @@ import (
 // DingTalk 钉钉键
 const DingTalk string = "dingtalk"
 
-// LoggerMsg 钉钉日志模板消息
-type LoggerMsg dingtalk.ActionCard
+// Sync 同步发送钉钉消息键
+const Sync string = "sync"
 
-func (LoggerMsg) Type() dingtalk.MsgType {
+// DingTalkLayout 钉钉消息布局
+type DingTalkLayout struct {
+	Banner string
+	Header string
+	Main   string
+	Footer string
+}
+
+func (d DingTalkLayout) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.Banner + d.Header + d.Main + d.Footer)
+}
+
+var _ json.Marshaler = DingTalkLayout{}
+
+// DingTalkLayout 用于钉钉消息的日志卡片
+type DingTalkCard struct {
+	Title       DingTalkLayout `json:"title"`
+	Text        DingTalkLayout `json:"text"`
+	SingleTitle string         `json:"singleTitle,omitempty"`
+	SingleURL   string         `json:"singleURL,omitempty"`
+}
+
+func (DingTalkCard) Type() dingtalk.MsgType {
 	return dingtalk.MsgActionCard
 }
 
-// DingTalkHook 钉钉机器人钩子
-type DingTalkHook struct {
-	*dingtalk.Bot
-	levels []logrus.Level // 日志等级，为空时视为全部等级
-}
-
-func (d *DingTalkHook) Levels() []logrus.Level {
-	if len(d.levels) != 0 {
-		return d.levels
-	}
-	return logrus.AllLevels
-}
-
-// Fire 发送钉钉消息，发送失败时会将错误写入日志
-func (d *DingTalkHook) Fire(entry *logrus.Entry) error {
-	if data, ok := entry.Data[DingTalk]; ok {
-		if data, ok := data.(string); ok && data == d.Bot.Name {
-			log := &LoggerMsg{}
-			if _, err := d.Bot.Fill(entry, log); err != nil {
-				return err
-			}
-			go func(logger *logrus.Logger, msg *LoggerMsg) {
-				if err := d.Bot.Send(msg); err != nil {
-					logger.Error(err)
-				}
-			}(entry.Logger, log)
-		}
-	}
-	return nil
-}
-
-var _ logrus.Hook = (*DingTalkHook)(nil)
-
-// Bind 将当前机器人绑定在日志上
-func (d *DingTalkHook) Bind(logger *logrus.Logger) *logrus.Entry {
-	return logger.WithField(DingTalk, d.Bot.Name)
-}
-
-// FirstLine 生成通知日志的首行
-func FirstLine(entry *logrus.Entry) string {
+// Header 生成通知日志的头部
+func Header(entry *logrus.Entry) string {
 	if value, ok := entry.Data["header"]; ok {
-		if header, ok := value.(string); ok {
-			return header
-		}
+		return fmt.Sprint(value)
 	}
 	b := &strings.Builder{}
 	b.WriteByte('[')
 	b.WriteString(strings.ToUpper(entry.Level.String()))
 	b.WriteByte(']')
 	if value, ok := entry.Data["title"]; ok {
-		if title, ok := value.(string); ok {
-			b.WriteByte(' ')
-			b.WriteString(title)
-		}
+		b.WriteByte(' ')
+		fmt.Fprint(b, value)
 	} else if entry.Caller != nil {
 		b.WriteByte(' ')
 		b.WriteString(filepath.Base(entry.Caller.File))
@@ -97,13 +80,91 @@ func TimeFormat(t time.Time) string {
 	return t.Format("2006-01-02 15:04:05")
 }
 
+// 默认消息模板
+var MsgTemplate = template.New("").Funcs(template.FuncMap{
+	"header": Header,
+	"strip":  stripmd.Strip,
+	"timef":  TimeFormat,
+	"prefix": Prefix,
+})
+
+var dingtalkCard dingtalk.Msg = DingTalkCard{
+	Title: DingTalkLayout{
+		Header: " {{header .}}\n",
+		Main:   "{{if strip .Message}}{{strip .Message}}\n{{end}}",
+		Footer: "{{timef .Time}}",
+	},
+	Text: DingTalkLayout{
+		Banner: "{{if .Data.banner}}![]({{.Data.banner}})\n{{end}}",
+		Header: "### {{header .}}\n\n",
+		Main:   "{{prefix .Message \"#### \"}}\n\n",
+		Footer: "###### {{timef .Time}}",
+	},
+	SingleTitle: "{{if .Data.button}}{{.Data.button}}{{end}}",
+	SingleURL:   "{{if .Data.url}}{{.Data.url}}{{end}}",
+}
+
+func init() {
+	err := filler.Parse(MsgTemplate, dingtalkCard)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// DingTalkHook 钉钉机器人钩子
+type DingTalkHook struct {
+	// 钉钉机器人
+	*dingtalk.Bot
+
+	// 自定义模板，用于将日志自动填充进钉钉 actionCard 类型消息
+	Template *template.Template
+
+	// 日志等级，为空时视为全部等级
+	levels []logrus.Level
+}
+
+func (d *DingTalkHook) Levels() []logrus.Level {
+	if len(d.levels) != 0 {
+		return d.levels
+	}
+	return logrus.AllLevels
+}
+
+// Send 发送钉钉消息，发送失败时会将错误写入日志
+func (d *DingTalkHook) Send(logger *logrus.Logger, msg dingtalk.Msg) {
+	if err := d.Bot.Send(msg); err != nil {
+		logger.Error(err)
+	}
+}
+
+// Fire 触发日志转发，当日志事件中指定的钉钉机器人与自身名称相同时转发
+func (d *DingTalkHook) Fire(entry *logrus.Entry) (err error) {
+	if entry.Data[DingTalk] == d.Bot.Name {
+		card := &DingTalkCard{}
+		if d.Template != nil {
+			err = filler.Fill(d.Template, entry, card)
+		} else {
+			err = filler.Fill(MsgTemplate, entry, card)
+		}
+		if err != nil {
+			return
+		}
+		if entry.Data[Sync] == true {
+			return d.Bot.Send(card)
+		}
+		go d.Send(entry.Logger, card)
+	}
+	return
+}
+
+var _ logrus.Hook = (*DingTalkHook)(nil)
+
+// Bind 将当前机器人绑定在日志上
+func (d *DingTalkHook) Bind(logger *logrus.Logger) *logrus.Entry {
+	return logger.WithField(DingTalk, d.Bot.Name)
+}
+
 // NewDingTalkHook 创建钉钉机器人钩子，日志等级为空时视为全部等级
 func NewDingTalkHook(bot *dingtalk.Bot, levels ...logrus.Level) *DingTalkHook {
-	bot.Funcs(template.FuncMap{"titlef": FirstLine, "prefix": Prefix, "stripmd": stripmd.Strip, "timef": TimeFormat}).Parse(LoggerMsg{
-		Title:       " {{titlef .}}\n{{if stripmd .Message}}{{stripmd .Message}}\n{{end}}{{timef .Time}}",
-		Text:        "{{if .Data.banner}}{{.Data.banner}}\n{{end}}### {{titlef .}}\n\n{{prefix .Message \"#### \"}}\n\n###### {{timef .Time}}",
-		SingleTitle: "{{if .Data.button}}{{.Data.button}}{{end}}",
-		SingleURL:   "{{if .Data.url}}{{.Data.url}}{{end}}",
-	})
 	return &DingTalkHook{Bot: bot, levels: levels}
 }
--- hook/go.mod
+++ hook/go.mod
@@ -3,13 +3,14 @@ module github.com/Drelf2018/exp/hook
 go 1.18
 
 require (
-	github.com/Drelf2018/dingtalk v0.0.0-20260119185921-eb58aad0f621
+	github.com/Drelf2018/dingtalk v0.0.0-20260202025804-7a9247527236
+	github.com/Drelf2018/exp/filler v0.0.0-20260129131937-a0f34b27691a
 	github.com/antonfisher/nested-logrus-formatter v1.3.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/writeas/go-strip-markdown v2.0.1+incompatible
 )
 
 require (
-	github.com/Drelf2018/req v0.0.0-20260119155603-2094703bdf97 // indirect
+	github.com/Drelf2018/req v0.0.0-20260202023602-73315c9061f0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
