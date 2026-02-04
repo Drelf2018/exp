e45ce05aefa2f0ab518558146279e410dc23e5ea
12138 <41439182+Drelf2018@users.noreply.github.com>
2026-02-04 16:45:21 +0800

    remove: 移除了非必须 filler 库

--- hook/dingtalk.go
+++ hook/dingtalk.go
@@ -5,65 +5,75 @@ import (
 	"fmt"
 	"path/filepath"
 	"strings"
-	"text/template"
-	"time"
 
 	"github.com/Drelf2018/dingtalk"
-	"github.com/Drelf2018/exp/filler"
 	"github.com/sirupsen/logrus"
 	stripmd "github.com/writeas/go-strip-markdown"
 )
 
-// DingTalk 钉钉键
-const DingTalk string = "dingtalk"
+const (
+	// DingTalkKey 用来指定发送消息机器人名称的键
+	DingTalkKey string = "dingtalk"
 
-// Sync 同步发送钉钉消息键
-const Sync string = "sync"
+	// SyncKey 用来同步发送日志消息的键，值为真时启用。在发送消息完成前会阻塞其他日志的打印，谨慎使用
+	SyncKey string = "sync"
 
-// DingTalkLayout 钉钉消息布局
-type DingTalkLayout struct {
-	Banner string
-	Header string
-	Main   string
-	Footer string
+	// BannerKey 设置日志消息头图的键，值为头图链接
+	BannerKey string = "banner"
+
+	// HeaderKey 设置日志消息头部的键
+	HeaderKey string = "header"
+
+	// TitleKey 设置日志消息标题的键
+	TitleKey string = "title"
+
+	// ButtonKey 设置日志消息跳转按钮文本的键
+	ButtonKey string = "button"
+
+	// URLKey 设置日志消息跳转按钮的键，值为跳转链接
+	URLKey string = "url"
+)
+
+// Sync 用来同步发送日志消息
+var Sync = logrus.Fields{SyncKey: true}
+
+// Banner 设置日志消息头图
+func Banner(url string) logrus.Fields {
+	return logrus.Fields{BannerKey: url}
 }
 
-func (d DingTalkLayout) MarshalJSON() ([]byte, error) {
-	return json.Marshal(d.Banner + d.Header + d.Main + d.Footer)
+// Button 设置日志消息跳转按钮
+func Button(text, url string) logrus.Fields {
+	return logrus.Fields{ButtonKey: text, URLKey: url}
 }
 
-var _ json.Marshaler = DingTalkLayout{}
-
-// DingTalkLayout 用于钉钉消息的日志卡片
-type DingTalkCard struct {
-	Title       DingTalkLayout `json:"title"`
-	Text        DingTalkLayout `json:"text"`
-	SingleTitle string         `json:"singleTitle,omitempty"`
-	SingleURL   string         `json:"singleURL,omitempty"`
+// CardLayout 日志卡片布局
+type CardLayout struct {
+	Banner  string // 头图
+	Header  string // 首行
+	Content string // 内容
+	Footer  string // 页脚
 }
 
-func (DingTalkCard) Type() dingtalk.MsgType {
+func (d CardLayout) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.Banner + d.Header + d.Content + d.Footer)
+}
+
+var _ json.Marshaler = CardLayout{}
+
+// LoggerCard 日志类型消息
+type LoggerCard struct {
+	Title       CardLayout `json:"title"`
+	Text        CardLayout `json:"text"`
+	SingleTitle string     `json:"singleTitle,omitempty"`
+	SingleURL   string     `json:"singleURL,omitempty"`
+}
+
+func (LoggerCard) Type() dingtalk.MsgType {
 	return dingtalk.MsgActionCard
 }
 
-// Header 生成通知日志的头部
-func Header(entry *logrus.Entry) string {
-	if value, ok := entry.Data["header"]; ok {
-		return fmt.Sprint(value)
-	}
-	b := &strings.Builder{}
-	b.WriteByte('[')
-	b.WriteString(strings.ToUpper(entry.Level.String()))
-	b.WriteByte(']')
-	if value, ok := entry.Data["title"]; ok {
-		b.WriteByte(' ')
-		fmt.Fprint(b, value)
-	} else if entry.Caller != nil {
-		b.WriteByte(' ')
-		b.WriteString(filepath.Base(entry.Caller.File))
-	}
-	return b.String()
-}
+var _ dingtalk.Msg = LoggerCard{}
 
 // Prefix 为每行字符串添加前缀
 func Prefix(s, prefix string) string {
@@ -75,40 +85,49 @@ func Prefix(s, prefix string) string {
 	return strings.Join(newParts, "\n")
 }
 
-// TimeFormat 格式化时间
-func TimeFormat(t time.Time) string {
-	return t.Format("2006-01-02 15:04:05")
-}
-
-// 默认消息模板
-var MsgTemplate = template.New("").Funcs(template.FuncMap{
-	"header": Header,
-	"strip":  stripmd.Strip,
-	"timef":  TimeFormat,
-	"prefix": Prefix,
-})
-
-var dingtalkCard dingtalk.Msg = DingTalkCard{
-	Title: DingTalkLayout{
-		Header: " {{header .}}\n",
-		Main:   "{{if strip .Message}}{{strip .Message}}\n{{end}}",
-		Footer: "{{timef .Time}}",
-	},
-	Text: DingTalkLayout{
-		Banner: "{{if .Data.banner}}![]({{.Data.banner}})\n{{end}}",
-		Header: "### {{header .}}\n\n",
-		Main:   "{{prefix .Message \"#### \"}}\n\n",
-		Footer: "###### {{timef .Time}}",
-	},
-	SingleTitle: "{{if .Data.button}}{{.Data.button}}{{end}}",
-	SingleURL:   "{{if .Data.url}}{{.Data.url}}{{end}}",
-}
-
-func init() {
-	err := filler.Parse(MsgTemplate, dingtalkCard)
-	if err != nil {
-		panic(err)
+// NewLoggerCard 新建日志类型消息
+func NewLoggerCard(entry *logrus.Entry) *LoggerCard {
+	message := entry.Message
+	header, exists := entry.Data[HeaderKey]
+	if !exists {
+		level := "[" + strings.ToUpper(entry.Level.String()) + "]"
+		if value, ok := entry.Data[logrus.ErrorKey]; ok {
+			header = level + " " + entry.Message
+			message = fmt.Sprint(value)
+		} else if value, ok := entry.Data[TitleKey]; ok {
+			header = level + " " + fmt.Sprint(value)
+		} else if entry.Caller != nil {
+			header = level + " " + filepath.Base(entry.Caller.File)
+		} else {
+			header = level
+		}
 	}
+	card := &LoggerCard{
+		Title: CardLayout{
+			Header: fmt.Sprintf(" %s\n", header),
+			Footer: entry.Time.Format("2006-01-02 15:04:05"),
+		},
+		Text: CardLayout{
+			Header: fmt.Sprintf("### %s\n\n", header),
+		},
+	}
+	card.Text.Footer = "###### " + card.Title.Footer
+	if content := stripmd.Strip(message); content != "" {
+		card.Title.Content = content + "\n"
+	}
+	if content := Prefix(message, "#### "); content != "" {
+		card.Text.Content = content + "\n\n"
+	}
+	if banner, exists := entry.Data[BannerKey]; exists {
+		card.Text.Banner = fmt.Sprintf("![](%s)\n", banner)
+	}
+	if button, exists := entry.Data[ButtonKey]; exists {
+		card.SingleTitle = fmt.Sprint(button)
+	}
+	if url, exists := entry.Data[URLKey]; exists {
+		card.SingleURL = fmt.Sprint(url)
+	}
+	return card
 }
 
 // DingTalkHook 钉钉机器人钩子
@@ -116,8 +135,8 @@ type DingTalkHook struct {
 	// 钉钉机器人
 	*dingtalk.Bot
 
-	// 自定义模板，用于将日志自动填充进钉钉 actionCard 类型消息
-	Template *template.Template
+	// 根据日志事件生成钉钉消息，值为空时使用内置日志类型消息生成函数
+	New func(*logrus.Entry) (dingtalk.Msg, error)
 
 	// 日志等级，为空时视为全部等级
 	levels []logrus.Level
@@ -138,30 +157,42 @@ func (d *DingTalkHook) Send(logger *logrus.Logger, msg dingtalk.Msg) {
 }
 
 // Fire 触发日志转发，当日志事件中指定的钉钉机器人与自身名称相同时转发
-func (d *DingTalkHook) Fire(entry *logrus.Entry) (err error) {
-	if entry.Data[DingTalk] == d.Bot.Name {
-		card := &DingTalkCard{}
-		if d.Template != nil {
-			err = filler.Fill(d.Template, entry, card)
+func (d *DingTalkHook) Fire(entry *logrus.Entry) error {
+	if entry.Data[DingTalkKey] == d.Bot.Name {
+		var msg dingtalk.Msg
+		if d.New != nil {
+			var err error
+			msg, err = d.New(entry)
+			if err != nil {
+				return err
+			}
 		} else {
-			err = filler.Fill(MsgTemplate, entry, card)
+			msg = NewLoggerCard(entry)
 		}
-		if err != nil {
-			return
+		if entry.Data[SyncKey] == true {
+			return d.Bot.Send(msg)
 		}
-		if entry.Data[Sync] == true {
-			return d.Bot.Send(card)
-		}
-		go d.Send(entry.Logger, card)
+		go d.Send(entry.Logger, msg)
 	}
-	return
+	return nil
 }
 
 var _ logrus.Hook = (*DingTalkHook)(nil)
 
 // Bind 将当前机器人绑定在日志上
 func (d *DingTalkHook) Bind(logger *logrus.Logger) *logrus.Entry {
-	return logger.WithField(DingTalk, d.Bot.Name)
+	return logger.WithField(DingTalkKey, d.Bot.Name)
+}
+
+// WithError 用于打印错误
+func (d *DingTalkHook) WithError(logger *logrus.Logger) func(title string, err error, fields ...logrus.Fields) {
+	return func(title string, err error, fields ...logrus.Fields) {
+		entry := logger.WithFields(logrus.Fields{DingTalkKey: d.Bot.Name, logrus.ErrorKey: err})
+		for _, field := range fields {
+			entry = entry.WithFields(field)
+		}
+		entry.Error(title)
+	}
 }
 
 // NewDingTalkHook 创建钉钉机器人钩子，日志等级为空时视为全部等级
--- hook/go.mod
+++ hook/go.mod
@@ -4,7 +4,6 @@ go 1.18
 
 require (
 	github.com/Drelf2018/dingtalk v0.0.0-20260202025804-7a9247527236
-	github.com/Drelf2018/exp/filler v0.0.0-20260129131937-a0f34b27691a
 	github.com/antonfisher/nested-logrus-formatter v1.3.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/writeas/go-strip-markdown v2.0.1+incompatible
